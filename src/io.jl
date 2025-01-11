47a0f1467a1aac26cff46bf616a6151164fc7606
Maarten Pronk <git@evetion.nl>
2025-01-11 21:05:56 +0000

    Full read/write support.

--- src/io.jl
+++ src/io.jl
@@ -4,12 +4,27 @@
 Write a geospatial table to a file. Like Arrow.write, but with geospatial metadata.
 Any kwargs are passed to Arrow.write.
 """
-function write(path, t; kwargs...)
-    projjson = ""
-    crs = Dict("crs" => projjson)
-    colmetadata =
-        Dict(:geometry => ["ARROW:extension:metadata" => JSON3.write(crs)])
-    Arrow.write(path, t; colmetadata, kwargs...)
+function write(path, t; geocolumns=GeoInterface.geometrycolumns(t), crs=GeoInterface.crs(t), encoding::AbstractEncoding=Interleaved(), kwargs...)
+
+    if isnothing(crs)
+        dcrs = Dict{String,String}()
+    else
+        pcrs = convert(Proj.CRS, crs)
+        jcrs = convert(ProjJSON, pcrs)
+        dcrs = Dict("crs" => GeoFormatTypes.val(jcrs))
+    end
+    ct = Tables.columntable(t)
+    colmetadata = Dict{Symbol,Vector{Pair{String,String}}}()
+    for column in geocolumns
+        column in Tables.columnnames(t) || error("Geometry column $column not found in table")
+        data = Tables.getcolumn(t, column)
+        T = nonmissingtype(Tables.columntype(t, column))
+        GeoInterface.isgeometry(T) || error("Geometry in $column must support the GeoInterface")
+        ct = merge(ct, NamedTuple{(column,)}((Wrapper.(Ref(encoding), data),)))
+
+        colmetadata[column] = ["ARROW:extension:metadata" => JSON3.write(dcrs)]
+    end
+    Arrow.write(path, ct; colmetadata, kwargs...)
 end
 
 """
@@ -19,7 +34,23 @@ Read a geospatial table from a file. Like Arrow.Table, but with geospatial metad
 Any kwargs are passed to Arrow.Table.
 """
 function read(path; kwargs...)
-    t = Arrow.Table(path; kwargs...)
-    meta = Arrow.getmetadata(t)
+    at = Arrow.Table(path; kwargs...)
+    t = DataFrame(at, copycols=false)
+
+    # set GeoInterface metadata
+    names = []
+    for (column, metadata) in DataAPI.colmetadata(t)
+        "ARROW:extension:name" in keys(metadata) || continue
+        startswith(metadata["ARROW:extension:name"], "geoarrow.") || continue
+        push!(names, Symbol(column))
+
+        "ARROW:extension:metadata" in keys(metadata) || continue
+        extmetadata = metadata["ARROW:extension:metadata"]
+        isempty(extmetadata) && continue
+        crs = get(JSON3.read(extmetadata), :crs, nothing)
+        isnothing(crs) || DataAPI.metadata!(t, "GEOINTERFACE:crs", crs)
+    end
+    isempty(names) || DataAPI.metadata!(t, "GEOINTERFACE:geometrycolumns", Tuple(names))
+
     return t
 end
--- src/type.jl
+++ src/type.jl
@@ -1,6 +1,8 @@
+# For reading
 struct Geometry{X,D,T,G}
     geom::G
 end
+Base.:(==)(x::Geometry{X,D,T,G}, y::Geometry{X,D,T,G}) where {X,D,T,G} = x.geom == y.geom
 Base.show(io::IO, x::Geometry{X,D,T}) where {X,D,T} = print(io, "$X geometry in $(D)D with eltype $T")
 Geometry{X,D,T}(x) where {X,D,T} = Geometry{X,D,T,typeof(x)}(x)
 Geometry{PointTrait}(x::Vararg{T,D}) where {T,D} = Geometry{PointTrait,D,T}(reinterpret(NTuple{D,T}, x))
@@ -13,9 +15,12 @@ Base.getindex(x::Geometry{PointTrait,D,T}, i) where {D,T} = Base.getindex(x.geom
 GeoInterface.isgeometry(::Type{<:Geometry}) = true
 GeoInterface.ncoord(_, ::Geometry{X,D}) where {X,D} = D
 GeoInterface.getcoord(::PointTrait, g::Geometry, i) = Base.getindex(g.geom, i)
+GeoInterface.getcoord(::PointTrait, g::Geometry{X,D,T,<:GeoFormatTypes.MixedFormat}, i) where {X,D,T} = getcoord(PointTrait(), g.geom, i)
 GeoInterface.geomtrait(::Geometry{X}) where {X} = X()
 GeoInterface.ngeom(_, g::Geometry) = length(g.geom)
+GeoInterface.ngeom(t, g::Geometry{X,D,T,<:GeoFormatTypes.MixedFormat}) where {X,D,T} = ngeom(t, g.geom)
 GeoInterface.getgeom(_, g::Geometry, i) = Base.getindex(g, i)
+GeoInterface.getgeom(t, g::Geometry{X,D,T,<:GeoFormatTypes.MixedFormat}, i) where {X,D,T} = getgeom(t, g.geom, i)
 
 childtrait(::LineStringTrait) = PointTrait
 childtrait(::LinearRingTrait) = PointTrait
@@ -23,3 +28,33 @@ childtrait(::PolygonTrait) = LinearRingTrait
 childtrait(::MultiPointTrait) = PointTrait
 childtrait(::MultiLineStringTrait) = LineStringTrait
 childtrait(::MultiPolygonTrait) = PolygonTrait
+
+# For writing
+abstract type AbstractEncoding end
+abstract type AbstractNativeEncoding <: AbstractEncoding end
+struct Seperated <: AbstractNativeEncoding end
+struct Interleaved <: AbstractNativeEncoding end
+struct WellKnownBinary <: AbstractEncoding end
+struct WellKnownText <: AbstractEncoding end
+
+struct Wrapper{E,T,N,G}
+    geom::G
+end
+Base.:(==)(x::Wrapper{E,T,N,G}, y::Wrapper{E,T,N,G}) where {E,T,N,G} = x.geom == y.geom
+Base.show(io::IO, ::Wrapper{E,T,G}) where {E,T,G} = print(io, "$T geometry encoded as $E")
+Wrapper(e::AbstractEncoding, x) = Wrapper{typeof(e),typeof(GeoInterface.geomtrait(x)),GeoInterface.ncoord(x),typeof(x)}(x)
+Wrapper(x) = Wrapper(Interleaved(), x)
+
+data(x::Wrapper{E,T,N,G}) where {E,T,N,G} = _coordinates(E(), T(), N, x.geom)
+data(x::Wrapper{WellKnownBinary,T,G}) where {T,G} = getwkb(x.geom).val
+data(x::Wrapper{WellKnownText,T,G}) where {T,G} = getwkt(x.geom).val
+
+_coordinates(::Interleaved, t::AbstractPointTrait, N, geom) = NTuple{N,Float64}(getcoord(t, geom))
+_coordinates(::Seperated, t::AbstractPointTrait, N, geom) = nt(NTuple{N,Float64}(getcoord(t, geom)))
+function _coordinates(E::AbstractNativeEncoding, t::AbstractGeometryTrait, N, geom)
+    map(x -> _coordinates(E, GeoInterface.geomtrait(x), N, x), getgeom(t, geom))
+end
+
+nt(x::NTuple{2,Float64}) = NamedTuple{(:x, :y)}(x)
+nt(x::NTuple{3,Float64}) = NamedTuple{(:x, :y, :z)}(x)
+nt(x::NTuple{4,Float64}) = NamedTuple{(:x, :y, :z, :m)}(x)
